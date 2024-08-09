c1ca54421a0b6992f32d8ffa856bf9b88d806d39
Vitoria Albuquerque <72430735+vitoria174@users.noreply.github.com>
2024-08-08 23:05:15 -0300

    Add files via upload

---
+++ exercicio01.sql
@@ -0,0 +1,34 @@
+create table produtos(
+idprodutos int not null,
+nome varchar (30),
+preco float,
+categoria varchar (30),
+primary key(idprodutos)
+)default charset = utf16;
+
+insert into produtos values
+('1','celular','1200.50','eletronicos'),
+('2','caderno','25.60','papelaria'),
+('3','mesa','650','casa'),
+('4','tenis','56.90','calçados'),
+('5','vestido','78.90','vestuario');
+
+select nome, preco from produtos
+where preco > 50;
+
+update produtos
+set preco = '45.90'
+where idprodutos ='4'
+limit 1; 
+
+select * from produtos;
+
+delete from produtos
+where idprodutos = '5';
+
+
+alter table produtos
+add column estoque int;
+
+alter table produtos
+change column preco preço float;
